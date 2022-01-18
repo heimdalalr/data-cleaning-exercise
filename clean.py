6cf26ed22eae48eba77f2936a9d54d9a5ba3cfda
Rae Carter <92331343+heimdalalr@users.noreply.github.com>
2022-01-17 19:40:36 -0500

    finished function to adjust columns

--- clean.py
+++ clean.py
@@ -30,4 +30,25 @@ def understand_data(df):
     print('\nData types following conversion:\n', df.types)
     print('\nMin year: ', df['year'].min())
     print('\nMax year: ', df['year'].max())
-    print('\nAvg year: ', df['year'].mean)
+    print('\nAvg year: ', df['year'].mean())
+    print('\nMean of height column per artist:\n', df.groupby('artist')['height'].transform('mean'))
+    print('\nExamine columns with word artist:\n', df.filter(like='artist'))
+    print('\nExamine columns with word year (case insensitive):\n', df.filter(regex="(?i)year"))
+
+def adjust_columns(df):
+    """ For practice of naming and dropping columns"""
+
+    print('\n\n**** Removing and Fixing Columns Section ****')
+    print('\nDrop first row (Not in place):\n', df.drop(0))
+    print('\nDrop id column:\n', df.drop('id', axis=1))
+    print('\nDrop multiple columns:\n', df.drop(columns=['id', 'year', 'height', 'artistRole', 'creditLine']))
+    print('\nDrop multiple rows:\n', df.drop(labels=[0, 1, 2]))
+    print('\nData columns:\n', df.columns)
+    print('\nData columns to lower (Not in place):\n', df.columns.str.lower())
+    data1 = pd.read_csv('artwork_sample.csv')
+    data1.columns = map(lambda x: x.lower(), data1.columns)
+    print('\nColumns to lowercase using function:\n', data1.columns)
+    data1.rename({"thumbnailUrl":"thumbnail"}, axis=1, inplace=True)
+    data1.columns = ['id', 'accessionNumber', 'artist', 'artistRole', 'artistId', 'title', 'dateText', 'medium', 'creditLine', 'year', 'acquisitionYear', 'dimensions', 'width', 'height', 'depth', 'units', 'inscription', 'thumbnailCopyright', 'thumbnail', 'url']
+    print('\nRenamed columns:\n', data1.columns)
+
